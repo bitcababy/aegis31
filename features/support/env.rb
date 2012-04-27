269772ced0bec36acea6d4706c97676ec53465a0
Meredith Lesly <bitcababy+github@gmail.com>
2012-04-27 13:52:21 -0400

    No idea if cucumber will work at all

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,13 +1,26 @@
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
+# require 'bundler'
+# begin
+#   Bundler.setup(:default, :development)
+# rescue Bundler::BundlerError => e
+#   $stderr.puts e.message
+#   $stderr.puts "Run `bundle install` to install missing gems"
+#   exit e.status_code
+# end
 
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
-require 'aegis31'
+# $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
+# require 'aegis31'
+# 
+# require 'rspec/expectations'
+# require 'cucumber/rails'
+# require 'pickle/world'
+# require 'capybara/cucumber'
+# require 'capybara/rspec'
+# Capybara.default_selector = :css
 
-require 'rspec/expectations'
+# Remove/comment out the lines below if your app doesn't have a database.
+# For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
+# begin
+#   DatabaseCleaner.strategy = :transaction
+# rescue NameError
+#   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+# end
